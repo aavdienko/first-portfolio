10641e342fd257d4fe564f84661fd6e1280b5a2c
aavdienko <asiavostok@seznam.cz>
2023-08-01 20:20:05 +0200

    added offset prop

--- src/header/nav/Nav.js
+++ src/header/nav/Nav.js
@@ -5,14 +5,10 @@ import { Link } from "react-scroll";
 export const Nav = () => {
   return (
     <div className={style.nav}>
-      <Link to={'main'} activeClass={style.active} spy={true} smooth={true} duration={450} className={style.navlinkItem}>Main</Link>
-      <Link to={'skills'} activeClass={style.active} spy={true} smooth={true} duration={450} className={style.navlinkItem}>Skills</Link>
-      <Link to={'projects'} activeClass={style.active} spy={true} smooth={true} duration={450} className={style.navlinkItem}>Projects</Link>
-      <Link to={'contacts'} activeClass={style.active} spy={true} smooth={true} duration={450} className={style.navlinkItem}>Contacts</Link>
-      {/* <a href=''>Main</a>
-      <a href='#skills'>Skills</a>
-      <a href='#projects'>Projects</a>
-      <a href='#contacts'>Contacts</a> */}
+      <Link to={'main'} activeClass={style.active} spy={true} smooth={true} offset={-70} duration={650} className={style.navlinkItem}>Main</Link>
+      <Link to={'skills'} activeClass={style.active} spy={true} smooth={true} offset={-70} duration={650} className={style.navlinkItem}>Skills</Link>
+      <Link to={'projects'} activeClass={style.active} spy={true} smooth={true} offset={-70} duration={650} className={style.navlinkItem}>Projects</Link>
+      <Link to={'contacts'} activeClass={style.active} spy={true} smooth={true} offset={-70} duration={650} className={style.navlinkItem}>Contacts</Link>
     </div>
   );
 }
